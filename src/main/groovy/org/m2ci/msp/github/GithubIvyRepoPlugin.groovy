c617641215a04ab0d1cebef2e61173ebfd592171
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-08-11 20:24:53 +0200

    init plugin

---
+++ src/main/groovy/org/m2ci/msp/github/GithubIvyRepoPlugin.groovy
@@ -0,0 +1,11 @@
+package org.m2ci.msp.github
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GithubIvyRepoPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+    }
+}
